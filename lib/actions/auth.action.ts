3c7190cf922fe104fe86d8c76f40d71c32cfbb70
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-04-11 12:43:42 +0530

    chore: add firebase and firebase-admin dependencies to package.json

---
+++ lib/actions/auth.action.ts
@@ -0,0 +1,12 @@
+'use server'
+
+export async function signUp(params: SignUpParams) {
+    const {uid, name, email} = params;
+
+    try {
+        
+    } catch (error) {
+        console.error('Error creating user:', error);
+
+    }
+}
